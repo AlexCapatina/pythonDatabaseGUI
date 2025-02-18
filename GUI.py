0125c90670f1c95ad39432c2cbc11d4c86ab2742
Alex <oravetzal@yahoo.com>
2025-02-18 15:34:49 +0200

    - the Login and Database dropdown functions have been separated into different frames - since there are different frames for these, username and password no longer have to checked after each step, therefore lowering down the number of code lines required - each frame only checks if the requirements from the previous frame have been met - load and connect definings no longer need to check for username and passwords - CRUD functions have not been modified yet, should still work

--- GUI.py
+++ GUI.py
@@ -15,12 +15,12 @@ class pythonDB:
 
         self.frames = {}
 
-        for Frs in (LoginScreen, DatabaseSelectorScreen, MainTableScreen):
+        for Frs in (LoginWindow, DatabaseSelectionWindow, MainTableScreen):
             frame = Frs(self.container, self)
             self.frames[Frs] = frame
             frame.grid(row=0, column=0, sticly="nsew")
 
-        self.show_frame(LoginScreen)
+        self.show_frame(LoginWindow)
 
         def show_frame(self, frame_class):
             frame = self.frames[frame_class]
@@ -29,7 +29,7 @@ class pythonDB:
         # self.database = None
         # self.student_ops = None
 
-class LoginScreen(tk.Frame):
+class LoginWindow(tk.Frame):
     def __init__(self, mainFrame, controller):
         super().__init__(mainFrame)
         self.controller = controller
@@ -50,12 +50,12 @@ class LoginScreen(tk.Frame):
         if username and password:
             self.controller.username = username
             self.controller.password = password
-            self.controller.show_frame(DatabaseSelectorScreen)
+            self.controller.show_frame(DatabaseSelectionWindow)
         else:
             messagebox.showerror("Error", "Please enter both username and password")
 
 
-class DatabaseSelectorScreen(tk.Frame):
+class DatabaseSelectionWindow(tk.Frame):
     def __init__(self, mainFrame, controller):
         super().__init__(mainFrame)
         self.controller = controller
@@ -70,75 +70,104 @@ class DatabaseSelectorScreen(tk.Frame):
         self.return_database_button.pack()
 
         # Display table
-        self.tree = ttk.Treeview(self)
-        self.tree.pack(expand=True, fill="both")
+        # self.tree = ttk.Treeview(self)
+        # self.tree.pack(expand=True, fill="both")
+        #
+        # # CRUD buttons
+        # frame = tk.Frame(self)
+        # frame.pack(pady=10)
+        #
+        # labels = ["Name", "Branch", "Roll", "Section", "Age"]
+        # self.entries = {}
+        #
+        # for i, labels in enumerate(labels):
+        #     tk.Label(frame, text=labels).grid(row=0, column=i)
+        #     entry = tk.Entry(frame, width=12)
+        #     entry.grid(row=1, column=i)
+        #     self.entries[labels] = entry
+        #
 
-        # CRUD buttons
-        frame = tk.Frame(self)
-        frame.pack(pady=10)
-
-        labels = ["Name", "Branch", "Roll", "Section", "Age"]
-        self.entries = {}
-
-        for i, labels in enumerate(labels):
-            tk.Label(frame, text=labels).grid(row=0, column=i)
-            entry = tk.Entry(frame, width=12)
-            entry.grid(row=1, column=i)
-            self.entries[labels] = entry
-
-        tk.Button(frame, text="Insert", command=self.insert_record, width=12).grid(row=0, column=0, padx=5)
-        tk.Button(frame, text="Update", command=self.update_record, width=12).grid(row=0, column=1, padx=5)
-        tk.Button(frame, text="Delete", command=self.delete_record, width=12).grid(row=0, column=2, padx=5)
-
-    def connect_database(self):
-        if not self.database_var.get():
-            messagebox.showerror("Error", "Please select a database first")
-            return
-
-        self.database = Database(self.username_entry.get(), self.password_entry.get(), self.database_var.get())
-        self.student_ops = TableOperations(self.database)
-        messagebox.showinfo("Success", f"Connected to {self.database_var.get()} successfully")
-
-        # self.load_database()
-        self.load_table()
 
     def load_database(self):
-        if not self.username_entry.get() or not self.password_entry.get():
-            messagebox.showerror("Error", "Please enter username and password first")
-            return
-
-        db_temp = Database(self.username_entry.get(), self.password_entry.get(), "")
-        databases = db_temp.get_databases()
+        temporaryDB = Database(self.controller.username, self.controller.password, "")
+        databases = temporaryDB.get_databases()
 
         if databases:
             self.database_dropdown["values"] = databases
         else:
-            messagebox.showerror("Error", "No databases found or incorrect credentials!")
+            messagebox.showerror("Error", "No databases elected or wrong login credentials")
 
-        if not hasattr(self, "return_database_button"):
-            return_database_button = tk.Button(self.root, text="Return databases", command=self.load_database)
-            return_database_button.pack()
-
-        if not hasattr(self, "connect_button"):
-            self.connect_button = tk.Button(self.root, text="Connect to Database", command=self.connect_database)
-            self.connect_button.pack()
-
-    def load_table(self):
-        rows, columns = self.student_ops.get_table_data()
-
-        if not self.student_ops:
-            messagebox.showerror("Error", "Not connected to any database")
+        if not self.database_var.get():
+            messagebox.showerror("Error", "Please select a database first")
             return
 
-        if not rows:
-            messagebox.showwarning("Warning", "No records found in STUDENT table")
+        # self.database = Database(self.username_entry.get(), self.password_entry.get(), self.database_var.get())
+        # self.student_ops = TableOperations(self.database)
+        # messagebox.showinfo("Success", f"Connected to {self.database_var.get()} successfully")
+        #
+        # # self.load_database()
+        # self.load_table()
 
-        self.tree["columns"] = columns
+    def connect_database(self):
+        selectedDB = self.database_var.get()
+        if not selectedDB:
+            messagebox.showerror("Error", "Database not selected. Please select a database and then you can continue.")
+            return
+        self.controller.database = Database(self.controller.username,self.controller.password, selectedDB)
+        self.controller.tableOperations = TableOperations(self.controller.database)
+
+        messagebox.showinfo("Success", f"You are now connected to {selectedDB}")
+        self.controller.show_frame(tableScreen)
+
+class tableScreen(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.tree = ttk.Treeview(self)
+        frameButton = tk.Frame(self)
+        frameButton.pack(pady=10)
+
+        tk.Button(frameButton, text="Insert", command=self.insert_record, width=12).grid(row=0, column=0, padx=5)
+        tk.Button(frameButton, text="Update", command=self.update_record, width=12).grid(row=0, column=1, padx=5)
+        tk.Button(frameButton, text="Delete", command=self.delete_record, width=12).grid(row=0, column=2, padx=5)
+
+        self.load_table()
+        # if not self.username_entry.get() or not self.password_entry.get():
+        #     messagebox.showerror("Error", "Please enter username and password first")
+        #     return
+        #
+        # db_temp = Database(self.username_entry.get(), self.password_entry.get(), "")
+        # databases = db_temp.get_databases()
+        #
+        # if databases:
+        #     self.database_dropdown["values"] = databases
+        # else:
+        #     messagebox.showerror("Error", "No databases found or incorrect credentials!")
+        #
+        # if not hasattr(self, "return_database_button"):
+        #     return_database_button = tk.Button(self.root, text="Return databases", command=self.load_database)
+        #     return_database_button.pack()
+        #
+        # if not hasattr(self, "connect_button"):
+        #     self.connect_button = tk.Button(self.root, text="Connect to Database", command=self.connect_database)
+        #     self.connect_button.pack()
+
+    def load_table(self):
+        rows, columns = self.controller.tableOperations.get_table_data()
+        self.tree["columns"] = [col[0] for col in columns]
         self.tree["show"] = "headings"
 
+        # if not self.student_ops:
+        #     messagebox.showerror("Error", "Not connected to any database")
+        #     return
+        #
+        # if not rows:
+        #     messagebox.showwarning("Warning", "No records found in STUDENT table")
+
         for col in columns:
-            self.tree.heading(col, text=col)
-            self.tree.column(col, width=100)
+            self.tree.heading(col[0], text=col)
+            self.tree.column(col[0], width=100)
 
         for item in self.tree.get_children():
             self.tree.delete(item)
