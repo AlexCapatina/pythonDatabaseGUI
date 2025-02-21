c4a011e2255fe906764796634d516fcee1f51298
Alex <oravetzal@yahoo.com>
2025-02-21 17:53:51 +0200

    - prepared Flask API to work online. Currently, the STUDENT table from the database can be seen online in a json file on a local host http://127.0.0.1:5000 - wrote the CRUD functions but not tested yet; will require to build a small website to test them

--- flask_api.py
+++ flask_api.py
@@ -4,14 +4,32 @@ from table_operations import TableOperations
 
 app =Flask(__name__)
 
-db = Database("your_user", "your_password", "your_database")
+db = Database("root", "BestPassword89", "testPython_db")
 student_ops = TableOperations(db)
 
+@app.route("/students", methods=["GET"])
+def get_students():
+    rows, columns = student_ops.get_table_data()
+    students = [dict(zip(columns, row)) for row in rows]
+    return jsonify(students)
 @app.route('/students', methods=["POST"])
 def insert_student():
     data = request.json
-    student_ops.insert_record(data["name"], data["branch"], data["roll"], data["section"], data["age"])
-    return jsonify({"message":"Student added successfully"})
+    values = (data["name"], data["branch"], data["roll"], data["section"], data["age"])
+    student_ops.insert_record(values)
+    return jsonify({"message": "Student added successfully"}), 201
+
+@app.route("/students/<int:roll>", methods=["PUT"])
+def update_student(roll):
+    data = request.json
+    values = (data["name"], data["branch"], data["roll"], data["section"], data["age"])
+    student_ops.update_record(values)
+    return jsonify({"message": "Student added successfully"}), 200
+
+@app.route("/students/<int:roll>", methods=["DELETE"])
+def delete_student(roll):
+    student_ops.delete_record(roll)
+    return jsonify({"message": "Student added successfully"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
